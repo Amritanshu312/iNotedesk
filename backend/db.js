d80da32fe48d1505d23e42fa47b3ea34db1e4cb6
Amritanshu312 <ffdarkrayon@gmail.com>
2024-01-12 12:06:13 +0800

    Remove .env file

--- backend/db.js
+++ backend/db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: '.env' });
 
-const mongoURI = `mongodb+srv://${process.env.BACKEND_NAME}:${process.env.BACKEND_PASSWORD}@ibook.8vqxeuj.mongodb.net/ibook`
-// const mongoURI = `mongodb://localhost:27017/Inotebook`
+// const mongoURI = `mongodb+srv://${process.env.BACKEND_NAME}:${process.env.BACKEND_PASSWORD}@ibook.8vqxeuj.mongodb.net/ibook`
+const mongoURI = `mongodb://localhost:27017/Inotebook`
 
 const connectTomongoose = () => {
   mongoose
